f6416f3f4ca994ecf267adac5690ffca01823b0d
Guilherme Muniz <guilherme.muniz91@gmail.com>
2023-05-18 18:15:07 -0300

    req1 feito

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Login from './pages/Login';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Search from './pages/Search';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <Switch>
+        <Route path="/album/:id" component={ Album } />
+        <Route path="/search" component={ Search } />
+        <Route path="/profile/edit" component={ ProfileEdit } />
+        <Route path="/profile" component={ Profile } />
+        <Route path="/favorites" component={ Favorites } />
+        <Route exact path="/" component={ Login } />
+        <Route path="*" component={ NotFound } />
+      </Switch>
+    );
   }
 }
 
